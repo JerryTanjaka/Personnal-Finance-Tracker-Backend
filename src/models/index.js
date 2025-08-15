4e5be7a98752fe2374f18147681ab76d66fb8457
TANJAKA Jerry Jacques <tanjakajerryjacques@gmail.com>
2025-08-15 19:04:16 +0300

    fet : add a test-crud

--- src/models/index.js
+++ src/models/index.js
@@ -1,27 +1,29 @@
-import { Sequelize } from 'sequelize';
-import User from './User.js';
-import Category from './Category.js';
-import Expense from './Expense.js';
-import Income from './Income.js';
+/** @format */
 
-const sequelize = new Sequelize('sequelizedb', 'postgres', 'king aff', {
-  host: 'localhost',
-  dialect: 'postgres',
-  logging: false,
+import { Sequelize } from "sequelize";
+import Category from "./Category.js";
+import Expense from "./Expense.js";
+import Income from "./Income.js";
+import User from "./User.js";
+
+const sequelize = new Sequelize("sequelizedb", "postgres", "king aff", {
+	host: "localhost",
+	dialect: "postgres",
+	logging: false,
 });
 
 const models = {
-  User: User(sequelize),
-  Category: Category(sequelize),
-  Expense: Expense(sequelize),
-  Income: Income(sequelize),
+	User: User(sequelize),
+	Category: Category(sequelize),
+	Expense: Expense(sequelize),
+	Income: Income(sequelize),
 };
 
 // Création des associations
 Object.values(models).forEach(model => {
-  if (model.associate) {
-    model.associate(models);
-  }
+	if (model.associate) {
+		model.associate(models);
+	}
 });
 
-export { sequelize, models };
+export { models, sequelize };
