96225f0b0aadeffd94bad7bfe04034e18a0034d7
TANJAKA Jerry Jacques <tanjakajerryjacques@gmail.com>
2025-08-26 22:12:37 +0300

    refactor: rename getSummary to getSummaryBetweenDates

--- src/routes/summary.route.js
+++ src/routes/summary.route.js
@@ -3,6 +3,6 @@ import requireAuth from "../middleware/auth.js";
 import * as summaryController from "../controllers/summary.controller.js";
 
 const summary = express.Router();
-summary.get("/", requireAuth, summaryController.getSummary);
+summary.get("/", requireAuth, summaryController.getSummaryBetweenDates);
 summary.get("/monthly",requireAuth, summaryController.getMonthlySummary); 
 export default summary;
