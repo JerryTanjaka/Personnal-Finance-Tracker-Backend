bf2e423d7ca1b6343f8a858128199647f6932dab
RRTRyan <tiaryanrzk@gmail.com>
2025-08-26 18:42:09 +0300

    feat: add user profile endpoint

--- src/routes/changePassword.route.js
+++ src/routes/changePassword.route.js
@@ -2,7 +2,9 @@ import express from 'express';
 const router = express.Router();
 import { changePassword } from '../controllers/auth.controller.js';
 import requireAuth from '../middleware/auth.js';
+import { getProfileInfo } from '../controllers/user.controller.js';
 
 router.post('/change-password', requireAuth, changePassword);
+router.get('/profile', requireAuth, getProfileInfo)
 
 export default router;
