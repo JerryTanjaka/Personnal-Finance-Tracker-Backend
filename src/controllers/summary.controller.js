67ff8d8f15911636b7f29ad991d7bc81922815d8
TANJAKA Jerry Jacques <tanjakajerryjacques@gmail.com>
2025-08-26 21:57:48 +0300

    feat: add get summary and getMonthlySummary route

--- src/controllers/summary.controller.js
+++ src/controllers/summary.controller.js
@@ -7,12 +7,44 @@ const Expense = db.Expense;
 const Sequelize = db.Sequelize;
 const getMonthlySummary = async (req, res) => {
 	try {
+		const { month } = req.query;
+
+		if (!month) {
+			return res.status(400).json({ message: "Month is required" });
+		}
+		const [yearNum, monthNum] = month.split("-").map(Number);
+		const startDate = new Date(yearNum, monthNum - 1, 1);
+		const endDate = new Date(yearNum, monthNum, 0, 23, 59, 59);
+		const totalIncome = await Income.sum("amount", {
+			where: { income_date: { [Op.between]: [startDate, endDate] } },
+		});
+
+		const totalExpense = await Expense.sum("amount", {
+			where: { date: { [Op.between]: [startDate, endDate] } },
+		});
+
+		res.status(200).json({
+			month: monthNum,
+			year: yearNum,
+			totalIncome: totalIncome || 0,
+			totalExpense: totalExpense || 0,
+			netSavings: (totalIncome || 0) - (totalExpense || 0),
+		});
+	} catch (error) {
+		console.error("Error fetching monthly summary:", error.message);
+		res.status(500).json({ message: "Internal server error" });
+	}
+};
+
+const getSummary = async (req, res) => {
+	try {
 		const { month, year } = req.query;
+		const startDate = req.params.startDate;
+		const endDate = req.params.endDate;
 		if (!month || !year) {
 			return res.status(400).json({ message: "Month and year are required" });
 		}
-		const startDate = new Date(year, month - 1, 1);
-		const endDate = new Date(year, month, 0, 23, 59, 59);
+
 		const totalIncome = await Income.sum("amount", {
 			where: {
 				date: {
@@ -39,4 +71,4 @@ const getMonthlySummary = async (req, res) => {
 		res.status(500).json({ message: "Internal server error" });
 	}
 };
-export { getMonthlySummary };
+export { getMonthlySummary, getSummary };
