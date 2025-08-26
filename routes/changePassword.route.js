c3c97cff346fae7c8e14d02ff22e15b1635c3164
Ilo <hei.iloniaina@gmail.com>
2025-08-26 12:43:28 +0300

    feat: implement change password route with authentication

--- routes/changePassword.route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { changePassword } = require('../controllers/auth.controller');
-const authMiddleware = require('../middleware/auth');
-
-// Route to change user's password
-router.post('/change-password', authMiddleware, changePassword);
-
-module.exports = router;
