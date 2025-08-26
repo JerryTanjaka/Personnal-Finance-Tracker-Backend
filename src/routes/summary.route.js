6d01447a3997eec9f2501c643f6e2f2b8f1526ae
TANJAKA Jerry Jacques <tanjakajerryjacques@gmail.com>
2025-08-26 22:15:31 +0300

    feat: add monthly alert route with requireAuth

--- src/routes/summary.route.js
+++ src/routes/summary.route.js
@@ -5,4 +5,5 @@ import * as summaryController from "../controllers/summary.controller.js";
 const summary = express.Router();
 summary.get("/", requireAuth, summaryController.getSummaryBetweenDates);
 summary.get("/monthly",requireAuth, summaryController.getMonthlySummary); 
+summary.get("/alerts",requireAuth, summaryController.getMonthlySummary);
 export default summary;
