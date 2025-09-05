477d7808a6893f0f4cf2920f4481121de700e98f
RRTRyan <tiaryanrzk@gmail.com>
2025-09-05 15:39:10 +0300

    fix: docs authentification

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -6,5 +6,5 @@ export const getProfileInfo = async (req, res) => {
     const userUUID = req.user?.id
 
     const userInfo = await db.User.findOne({ where: { id: userUUID }, attributes: { exclude: ['password', 'id'] } }).catch(rej => res.status(500).json({ message: "Failed to fetch user info", error: rej }))
-    return res.status(200).json({ userInfo })
+    return res.status(200).json( userInfo )
 }
