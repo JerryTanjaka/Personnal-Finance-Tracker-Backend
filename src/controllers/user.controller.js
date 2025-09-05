7923713211c3f234758b0ab4e4ab14758760e97a
RRTRyan <tiaryanrzk@gmail.com>
2025-09-05 15:32:40 +0300

    fix: remove unneeded fields

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,20 +5,6 @@ import db from "../models/index.js";
 export const getProfileInfo = async (req, res) => {
     const userUUID = req.user?.id
 
-    const userInfo = await db.User.findOne({ where: { id: userUUID }, attributes: {exclude: ['password']} }).catch(rej => res.status(500).json({ message: "Failed to fetch user info", error: rej }))
-    const expenseInfo = await db.Expense.findAll({
-        where: { user_id: userUUID },
-        attributes: [
-            [sequelize.fn('COUNT', sequelize.col('id')), 'expenseCount'],
-            [sequelize.fn('SUM', sequelize.col('amount')), 'expenseAmount'],
-        ]
-    }).catch(rej => res.status(500).json({ message: "Failed to fetch expense info", error: rej }))
-    const cateogryInfo = await db.Category.findAll({
-        where: { [Op.and]: { user_id: userUUID, is_default: false } },
-        attributes: [
-            [sequelize.fn('COUNT', sequelize.col('id')), 'customCategory'],
-        ]
-    }).catch(rej => res.status(500).json({ message: "Failed to fetch expense info", error: rej }))
-
-    return res.status(200).json({ userInfo, expenseInfo, cateogryInfo })
+    const userInfo = await db.User.findOne({ where: { id: userUUID }, attributes: { exclude: ['password', 'id'] } }).catch(rej => res.status(500).json({ message: "Failed to fetch user info", error: rej }))
+    return res.status(200).json({ userInfo })
 }
