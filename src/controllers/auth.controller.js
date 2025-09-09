a0707453a81c2abec50018bd3fb16c25393bddeb
Vaniaina <hei.vaniaina@gmail.com>
2025-09-09 11:24:17 +0300

    feat: remember me option added

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -6,9 +6,9 @@ import createDefaultCategory from '../utils/createBaseCategory.js';
 const router = Router();
 
 // Utils
-const issueTokens = async (user) => {
+const issueTokens = async (user, rememberMe) => {
     const accessToken = signAccessToken({ sub: user.id, email: user.email });
-    const refreshToken = signRefreshToken({ sub: user.id });
+    const refreshToken = signRefreshToken({ sub: user.id }, { expiresIn: rememberMe ? '30d' : process.env.JWT_REFRESH_EXPIRES || '7d' });
 
     const decoded = verifyRefreshToken(refreshToken);
     const expires_at = new Date(decoded.exp * 1000);
@@ -33,7 +33,7 @@ export const registerUser = async (req, res) => {
         if (exists) return res.status(409).json({ message: 'email already used' });
 
         const user = await db.User.create({ email, password });
-        const userTokens = await issueTokens(user);
+        const userTokens = await issueTokens(user, false);
 
         await createDefaultCategory(user.id)
 
@@ -48,14 +48,14 @@ export const registerUser = async (req, res) => {
 // Login
 export const login = async (req, res) => {
     try {
-        const { email, password } = req.body;
+        const { email, password, rememberMe } = req.body;
         const user = await db.User.findOne({ where: { email } });
         if (!user) return res.status(401).json({ message: 'Invalid Credentials' });
 
         const ok = await user.checkPassword(password);
         if (!ok) return res.status(401).json({ message: 'Invalid Credentials' });
 
-        const tokens = await issueTokens(user);
+        const tokens = await issueTokens(user, rememberMe);
         return res.json({ user: { id: user.id, email: user.email }, ...tokens });
     } catch (e) {
         return res.status(500).json({ message: 'Server Error', error: e.message });
