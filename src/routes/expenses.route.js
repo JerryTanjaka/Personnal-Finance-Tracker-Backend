5d2607ede4e8c929042e10ba0cf2358871730313
RRTRyan <tiaryanrzk@gmail.com>
2025-09-11 15:05:33 +0300

    fix: catch server crashing error when receipt file is exceeding limit (#40)

--- src/routes/expenses.route.js
+++ src/routes/expenses.route.js
@@ -1,13 +1,13 @@
 import express from 'express'
 import { createExpense, deleteExpense, getAllExpenses, getSpecificExpense, updateExpense } from '../controllers/expenses.controller.js'
-import { uploadReceipt } from '../middleware/uploadReceipt.js'
+import { uploadReceipt, MulterErrorHandler } from '../middleware/uploadReceipt.js'
 
 const expenses = express.Router()
 
 expenses.get('/', getAllExpenses)
 expenses.get('/:id', getSpecificExpense)
-expenses.post('/', uploadReceipt.single('receipt'), createExpense)
-expenses.put('/:id', uploadReceipt.single('receipt'), updateExpense)
+expenses.post('/', uploadReceipt, MulterErrorHandler, createExpense)
+expenses.put('/:id', uploadReceipt, MulterErrorHandler, updateExpense)
 expenses.delete('/:id', deleteExpense)
 
 export default expenses
