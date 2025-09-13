9cd3ef7cfe2e21488d6a6a8c4a2331eb1f7ef0e2
TANJAKA Jerry Jacques <tanjakajerryjacques@gmail.com>
2025-09-13 08:36:31 +0300

    fix : ssl authorization

--- src/config/db.js
+++ src/config/db.js
@@ -6,11 +6,19 @@ dotenv.config();
 let sequelize;
 
 if (process.env.DATABASE_URL) {
+  
   sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
     logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, 
+      },
+    },
   });
 } else {
+
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
