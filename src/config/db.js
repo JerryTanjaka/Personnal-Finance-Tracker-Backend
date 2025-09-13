1b938fdae04b82ab333f4b275c6a7176036235db
TANJAKA Jerry Jacques <tanjakajerryjacques@gmail.com>
2025-09-13 19:11:11 +0300

    feat : add ssl options

--- src/config/db.js
+++ src/config/db.js
@@ -12,18 +12,24 @@ if (process.env.DATABASE_URL) {
 		dialect: "postgres",
 		logging: false,
 		dialectOptions: {
-		
+			ssl: {
+				require: true,
+				rejectUnauthorized: false, 
+			},
 		},
 	});
 } else {
-	sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-		host: process.env.DB_HOST,
-		port: process.env.DB_PORT,
-		dialect: "postgres",
-		logging: false,
-	});
-
+	sequelize = new Sequelize(
+		process.env.DB_NAME,
+		process.env.DB_USER,
+		process.env.DB_PASS,
+		{
+			host: process.env.DB_HOST,
+			port: process.env.DB_PORT,
+			dialect: "postgres",
+			logging: false,
+		}
+	);
 }
+
 export default sequelize;
-
-
