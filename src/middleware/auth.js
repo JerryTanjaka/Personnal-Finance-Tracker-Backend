5a9192f9b34e1f1e15fe12bd75da809fe7a3adb8
TANJAKA Jerry Jacques <tanjakajerryjacques@gmail.com>
2025-09-13 19:02:07 +0300

    fix : fix cookies not transfer on different domain

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,9 +1,12 @@
-import { verifyAccessToken } from '../utils/jwt.js';
-import express from "express";
 import cookieParser from 'cookie-parser';
+import { verifyAccessToken } from '../utils/jwt.js';
 
-const requireAuth = express().use(cookieParser(process.env.COOKIE_SECRET || 'set_a_cookie_secret_please'), (req, res, next) => {
-  const auth = cookieParser.signedCookie(req.signedCookies["access_token"], process.env.COOKIE_SECRET || 'set_a_cookie_secret_please') || null;
+const requireAuth = (req, res, next) => {
+  const auth = cookieParser.signedCookie(
+    req.signedCookies["access_token"],
+    process.env.COOKIE_SECRET || 'set_a_cookie_secret_please'
+  ) || null;
+
   const [scheme, token] = auth?.split(' ') || [null, null];
 
   if (scheme !== 'Bearer' || !token) {
@@ -17,6 +20,6 @@ const requireAuth = express().use(cookieParser(process.env.COOKIE_SECRET || 'set
   } catch (e) {
     return res.status(401).json({ message: 'Token invalide ou expiré' });
   }
-});
+};
 
 export default requireAuth;
